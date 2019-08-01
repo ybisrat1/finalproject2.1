04a7055642d0382c2176babb15f803c3a9c55a5a
Yusef Bisrat <yb4820@att.com>
2019-08-01 13:58:48 -0400

    separted again

--- vagrant/test.py
+++
@@ -1,6 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from database_setup import Base, Restaurant, MenuItem
-engine = create_engine('sqlite:///restaurantmenu.db')
-BaseMetadata.bind= egine
-DBSession = sessionmaker(bind = engine)
